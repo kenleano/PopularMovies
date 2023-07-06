8c40a8fb70291cbf9bc7313a774a678183110390
Kenneth <kenleano@gmail.com>
2023-07-06 14:39:33 -0700

    Initial

---
+++ app/src/main/java/com/example/popularmovies/Movie.java
@@ -0,0 +1,13 @@
+package com.example.popularmovies;
+
+public class Movie {
+    private String title;
+    private String poster;
+    private Double rating;
+    private String overview;
+
+    public Movie(String title, String poster, Double rating, String overview ){
+
+    }
+
+}
